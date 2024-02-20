5df584fc3eab092390db46fab950902dd61e4f10
bathulapraka <bathulaprakash406@gmail.com>
2024-02-20 12:03:42 +0530

    Remove nested repository entry

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import ProductForm from './Flipkart details/Flipkart';
+function App() {
+  return (
+    <div className="App">
+    <ProductForm/>
+    </div>
+  );
+}
+
+export default App;
